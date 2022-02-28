7b81404319cd7d41c363d58d29eb5e378c09adad
Shaadi Alfred <shaadi.alfred@gmail.com>
2022-02-28 23:17:36 +0200

    Init

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V> : IEnumerable<KeyValuePair<K, V>>
+    {
+        private KeyValuePair<K, V>?[] arrayBucket;
+        private int capacity = 8;
+        public int Count { get; private set; }
+
+        public MyDictionary()
+        {
+            arrayBucket = new KeyValuePair<K, V>?[capacity];
+        }
+
+        private int GetHash(K key)
+        {
+            return Math.Abs(key.GetHashCode()) % capacity;
+        }
+
+
+        public void Add(K key, V value)
+        {
+            int index = GetHash(key);
+
+            if (Count >= 0.75 * capacity)
+            {
+                IncreaseCapacity();
+            }
+
+            while (arrayBucket[index].HasValue)
+            {
+                if (arrayBucket[index].Value.Key.Equals(key))
+                {
+                    throw new InvalidOperationException($"The key [{key}] is already assigned to a value");
+                }
+
+                index++;
+            }
+
+            var kVP = new KeyValuePair<K, V>(key, value);
+            arrayBucket[index] = new KeyValuePair<K, V>?(kVP);
+
+            Count++;
+        }
+
+        private void IncreaseCapacity()
+        {
+            capacity *= 2;
+
+            Count = 0;
+
+            var tempOldArrayBucket = arrayBucket;
+
+            arrayBucket = new KeyValuePair<K, V>?[capacity];
+
+            foreach (var keyValuePair in tempOldArrayBucket)
+            {
+                if (keyValuePair.HasValue)
+                {
+                    Add(keyValuePair.Value.Key, keyValuePair.Value.Value);
+                }
+            }
+        }
+
+        public V Get(K key)
+        {
+            foreach (var kVP in arrayBucket)
+            {
+                if (kVP.HasValue)
+                {
+                    if (kVP.Value.Key.Equals(key))
+                    {
+                        return kVP.Value.Value;
+                    }
+                }
+            }
+
+            throw new InvalidOperationException($"Key [{key}] was not found");
+        }
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            foreach (var keyValuePair in arrayBucket)
+            {
+                if (keyValuePair.HasValue)
+                {
+                    yield return keyValuePair.Value;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public V this[K key]
+        {
+            get { return Get(key); }
+            set { Add(key, value); }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myDictionary = new MyDictionary<string, int>();
+            string index = "Four";
+
+            myDictionary.Add("One", 1);
+            myDictionary.Add("Two", 2);
+            myDictionary.Add("Three", 3);
+            myDictionary.Add("Four", 4);
+
+            Console.WriteLine("-- Testing Get method");
+            Console.WriteLine($"[{index}] => {myDictionary.Get(index)}");
+            Console.WriteLine();
+
+            myDictionary.Add("Five", 5);
+
+            Console.WriteLine("-- Increasing Capacity");
+            myDictionary.Add("Six", 6);
+            myDictionary.Add("Seven", 7);
+            myDictionary.Add("Eight", 8);
+            myDictionary.Add("Nine", 9);
+            myDictionary.Add("Ten", 10);
+            myDictionary.Add("Eleven", 11);
+            myDictionary.Add("Twelve", 12);
+            Console.WriteLine();
+
+            // Adding same value with different key
+            myDictionary.Add("Eight Again", 8);
+
+            Console.WriteLine("Using indexer");
+            index = "Seven";
+            Console.WriteLine($"[{index}] => {myDictionary[index]}");
+
+            index = "Thirteen";
+            myDictionary[index] = 13;
+            Console.WriteLine($"[{index}] => {myDictionary[index]}");
+            Console.WriteLine();
+
+            Console.WriteLine("-- Adding the same key");
+            try
+            {
+                myDictionary[index] = 13;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("-- Exception");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("-- foreach");
+            foreach (var kVP in myDictionary)
+            {
+                Console.WriteLine($"[{kVP.Key}] => [{kVP.Value}]");
+            }
+        }
+    }
+}
